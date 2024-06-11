5a5a1526ffa014af79555f71e5a70d371e67e163
The2Losers <prnvinformal@gmail.com>
2024-06-11 20:57:01 +0530

    backend setup - second commit

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask,request,jsonify
+# 
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  # Import the CORS class
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/equipdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
